e4e6b9162ebc5384b21690bce3e7b483b0f0842e
Steve Fallet <steve.fallet@gmail.com>
2020-10-25 16:08:37 +0100

    Ajout fichiers de base

---
+++ liste1.js
@@ -0,0 +1,36 @@
+/**
+ * Exercice Liste - étape 1
+ *
+ * Sur click du bouton "Changer le contenu de la liste" :
+ *
+ *     1. Changer le style CSS de la liste, list-style-type, en carré : square
+ *
+ *     2. Remplacer le texte de tous les éléments de la liste par « CLICK ME n »
+ *        n remprésentant la position de l'élément dans la liste : 1, 2, 3, ...
+ *
+ *     3. Affecter un évènement click à tous les éléments de la liste qui
+ *        ouvrira une fenêtre d’avertissement (alert) affichant le contenu de
+ *        de l'élément cliqué.
+ *
+ * Contraintes :
+ *
+ *     - Interdiction de modifier le code du document HTML
+ *
+ * @author      Steve Fallet <steve.fallet@divtec.ch>
+ * @version     3
+ * @since       2014-03-31
+ *
+ * http://usejsdoc.org/
+ */
+
+//http://www.jslint.com/help.html
+/*jslint this:true */
+/*jslint es6, browser, devel, for, single, multivar*/
+/*global window, document, alert, performance */
+
+//Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
+(function () {
+    "use strict";
+
+
+}()); //Main IIFE
---
+++ liste2.js
@@ -0,0 +1,54 @@
+/**
+ * Exercice Liste - étape 2
+ *
+ * Sur click du bouton "Ajouter" :
+ *
+ *     1. Valider que le champs texte n'est pas vide. S'il est vide afficher
+ *        "Entrez un article !" dans une alert et terminer le script.
+ *     2. Récupérer le texte entré par l’utilisateur et l’ajouter comme
+ *        nouvel élément <li> à la fin de la liste.
+ *     3. Vider le champ texte
+ *
+ * Contraintes :
+ *     - Interdiction de modifier le document HTML
+ *     - Utiliser les méthodes createElement() et  appendChild() pour créer
+ *       et ajouter des éléments à la liste.
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * Exercice Liste - Etape 3
+ *
+ *    1. En JavaScript, ajouter un bouton "Supprimer" à la fin du paragraphe
+ *       contenant le champ texte et le bouton "Ajouter".
+ *
+ *    2. Sur click du nouveau bouton "Supprimer" supprimer le dernier élément
+ *       de la liste.
+ *
+ *    3. Ajouter un événement double click à la liste <ul> :
+ *        1. Récupérer l'élément <li> double clické (target) dans la liste <ul>
+ *        2. Supprimer l'élément <li> double clické
+ *
+ * Contraintes :
+ *    - Interdiction de modifier le document HTML
+ *    - Utiliser  cloneNode(false) pour créer le bouton supprimer en clonant
+ *      le bouton ajouter, puis en modifiant la valeur du bouton.
+ *    - Utiliser la méthode remove() pour supprimer des éléments
+ *
+ * @author      Steve Fallet <steve.fallet@divtec.ch>
+ * @version     3
+ * @since       2014-03-31
+ *
+ * http://usejsdoc.org/
+ */
+
+//http://www.jslint.com/help.html
+/*jslint this:true */
+/*jslint es6, browser, devel, for, single, multivar*/
+/*global window, document, alert, performance */
+
+//Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
+(function () {
+    "use strict";
+
+
+}()); //Main IIFE
